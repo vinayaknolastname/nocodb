dd44aaf424f9869d538c1f1a5edb0ee82587e45a
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-10-19 09:46:21 +0000

    feat: betaToggles menu

--- packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
+++ packages/nc-gui/components/smartsheet/column/EditOrAdd.vue
@@ -58,7 +58,7 @@ const { $e } = useNuxtApp()
 
 const { appInfo } = useGlobal()
 
-const { betaFeatureToggleStates, toggleBetaFeature } = useBetaFeatureToggle()
+const { isFeatureEnabled } = useBetaFeatureToggle()
 
 const { openedViewsTab } = storeToRefs(useViewsStore())
 
@@ -121,13 +121,9 @@ const isColumnTypeOpen = ref(false)
 const geoDataToggleCondition = (t: { name: UITypes }) => {
   if (!appInfo.value.ee) return true
 
-  return betaFeatureToggleStates[BetaFeatures.ENABLE_GEO_COLUMN]
-    ? betaFeatureToggleStates[BetaFeatures.ENABLE_GEO_COLUMN]
-    : !t.name.includes(UITypes.GeoData)
-}
+  const isColEnabled = isFeatureEnabled(FEATURE_FLAG.GEODATA_COLUMN)
 
-const toggleGeoData = () => {
-  toggleBetaFeature(BetaFeatures.ENABLE_GEO_COLUMN)
+  return isColEnabled || !t.name.includes(UITypes.GeoData)
 }
 
 const showDeprecated = ref(false)
@@ -438,7 +434,6 @@ const isFullUpdateAllowed = computed(() => {
           :placeholder="`${$t('objects.field')} ${$t('general.name').toLowerCase()} ${isEdit ? '' : $t('labels.optional')}`"
           :disabled="isKanban || readOnly || !isFullUpdateAllowed"
           @input="onAlter(8)"
-          @dblclick="toggleGeoData"
         />
       </a-form-item>
 
